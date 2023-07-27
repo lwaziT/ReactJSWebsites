b059a5c9a760401c75e95061ea74ab7f447d44d4
lwazi <toboselwazi9@gmail.com>
2023-07-27 15:50:58 +0200

    1.Edited font styling. 2.Added search bar

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,7 @@
 import React from "react"
 import "./Hero.css"
+import {HiLocationMarker} from 'react-icons/hi'
+import CountUp from "react-countup"
 
 const Hero = () => {
     return (
@@ -9,6 +11,7 @@ const Hero = () => {
                 {/* Left side */}
                 <div className="flexColStart hero-left">
                     <div className="hero-title">
+                        <div className="orange-circle"/>
                         <h1>
                             Discover <br/> Most Suitable <br/> Property
                         </h1>
@@ -19,11 +22,42 @@ const Hero = () => {
                         <span>Forget all difficulties in finding a residence for you</span>
                     </div>
 
-                    <div className="search-bar">
-                        search bar
+                    <div className="flexCenter search-bar">
+                        <HiLocationMarker color="var(--blue)" size={25}/>
+                        <input type="text"/>
+                        <button className="button">Search</button>
                     </div>
+
+                    <div className="flexCenter stats">
+                        <div className="flexColCenter stat">
+                            <span>
+                                <CountUp start={8800} end={9000} duration={4}/>
+                                <span>+</span>
+                            </span>
+                            <span className="secondaryText">Premium Product</span>
+                        </div>
+
+                        <div className="flexColCenter stat">
+                            <span>
+                                <CountUp start={1500} end={2000} duration={4}/>
+                                <span>+</span>
+                            </span>
+                            <span className="secondaryText">Happy Customer</span>
+                        </div>
+
+                        <div className="flexColCenter stat">
+                            <span>
+                                <CountUp end={28} duration={4}/>
+                                <span>+</span>
+                            </span>
+                            <span className="secondaryText">Awards Winning</span>
+                        </div>
+                    </div>
+
                 </div>
+
                 {/* Right side */}
+
                 <div className="flexCenter hero-right">
                     <div className="image-container">
                         <img src="./hero-image.png" alt="" />
