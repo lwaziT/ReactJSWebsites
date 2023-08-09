a50da0ad4b3e5b2ae7d626eb800a3c8f120933d6
lwazi <toboselwazi9@gmail.com>
2023-08-09 16:09:01 +0200

    Added GetStarted section

---
+++ src/components/GetStarted/GetStarted.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import './GetStarted.css'
+
+const GetStarted = () => {
+    return (
+        <section className="g-wrapper">
+            <div className="paddings innerwidth g-container">
+                <div className="flexColCenter inner-container">
+                    <span className="primaryText">Get Started with Homyz</span>
+                    <span className="secondaryText">
+                        Subscribe and find super attractive price quotes from Homyz
+                        <br/>
+                        Find your residence soon
+                    </span>
+                    <button className="button">
+                        <a href="mailto:">Get Started</a>
+                    </button>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default GetStarted;
