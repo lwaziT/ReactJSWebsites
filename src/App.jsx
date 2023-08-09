ec50e1184fb669fbeead1ed37d8207caabe6ffa0
lwazi <toboselwazi9@gmail.com>
2023-08-09 15:13:21 +0200

    Added Contact section - image, modes and styling

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import './App.css'
 import Companies from "./components/Companies/Companies";
 import Residencies from "./components/Residencies/Residencies";
 import Value from "./components/Value/Value";
+import Contact from "./components/Contact/Contact";
+
 function App() {
   return (
     <div className="App">
@@ -15,6 +17,7 @@ function App() {
       <Companies/>
       <Residencies/>
       <Value/>
+      <Contact/>
     </div>
   );
 }
